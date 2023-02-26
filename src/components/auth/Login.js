dc7e719fc4bd8f1ee66aac11b9e3256c8150728e
Ayush <89254576+AyushPurohit31@users.noreply.github.com>
2023-02-26 20:24:06 +0530

    Update Login.js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "/api/user/login",
+        "https://web-whatschat-api.onrender.com/api/user/login",
         { email, password },
         config
       );
