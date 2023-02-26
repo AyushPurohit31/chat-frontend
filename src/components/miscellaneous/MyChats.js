2ad4b44599d7ef25b8ea0138636943062c462420
Ayush <89254576+AyushPurohit31@users.noreply.github.com>
2023-02-26 20:26:43 +0530

    Update MyChats.js

--- src/components/miscellaneous/MyChats.js
+++ src/components/miscellaneous/MyChats.js
@@ -22,7 +22,7 @@ const MyChats = ({fetchAgain}) => {
         },
       };
 
-      const { data } = await axios.get("/api/chat", config);
+      const { data } = await axios.get("https://web-whatschat-api.onrender.com/api/chat", config);
       setChats(data);
     } catch (error) {
       alert("Error occured!")
